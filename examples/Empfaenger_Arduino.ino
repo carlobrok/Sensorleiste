493a6a71245c41e09fc547a89a686c703feec23a
carlobrok <carlo.brokering@outlook.de>
2019-12-15 16:34:42 +0100

    Examples Ordner angelegt

---
+++ examples/Empfaenger_Arduino.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include "Sensorleiste.h"
+
+Farbsensor sensoren[4];
+
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master)
+  init_sensorleiste(4, 0x08);
+  Serial.begin(115200);     // Der serielle Monitor wird aktiviert.
+}
+
+void loop()
+{
+  sensoren_auslesen(sensoren);
+
+  for(int i = 0; i < 4; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i+1);
+    Serial.print(": ");
+    Serial.print(sensoren[i].helligkeit);
+    Serial.print(" ");
+    Serial.print(sensoren[i].rot_prozent);
+    Serial.print(" ");
+    Serial.print(sensoren[i].gruen_prozent);
+    Serial.print(" ");
+    Serial.print(sensoren[i].blau_prozent);
+    Serial.print("\t");
+  }
+}
